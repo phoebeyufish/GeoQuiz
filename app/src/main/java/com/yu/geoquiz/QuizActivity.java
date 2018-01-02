b0fd662fc5c543982e7707b5ab90d38a17b39dd1
phoebeyufish <linphie@163.com>
2018-01-02 16:45:00 +0800

    书本第一章

--- app/src/main/java/com/yu/geoquiz/QuizActivity.java
+++ app/src/main/java/com/yu/geoquiz/QuizActivity.java
@@ -1,80 +1,43 @@
-package com.yu.geoquiz;
+package com.chenxi.geoquiz;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
-    private static final String TAG = "QuizActivity";
-    private static final String KEY_INDEX = "index";
-
-    Button mTrueBtn;
-    Button mFalseBtn;
-
-    int mIndex = 0;
-
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        outState.putInt(KEY_INDEX, mIndex);
-    }
-
-    @Override
-    protected void onRestoreInstanceState(Bundle savedInstanceState) {
-        super.onRestoreInstanceState(savedInstanceState);
-        if(savedInstanceState != null) {
-            mIndex = savedInstanceState.getInt(KEY_INDEX, -1);
-        }
-    }
-
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.i(TAG, "onStart called");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-    }
-
-
+    private Button mTrueBtn;
+    private Button mFalseBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_quiz);
 
-        if(savedInstanceState != null) {
-            mIndex = savedInstanceState.getInt(KEY_INDEX, -1);
-        }
+        //mTrueBtn = (Button) findViewById(R.id.true_button);
+        mFalseBtn = (Button) findViewById(R.id.false_button);
 
-        mTrueBtn = (Button) findViewById(R.id.trueBtn);
         mTrueBtn.setOnClickListener(new View.OnClickListener() {
+
             @Override
-            public void onClick(View view) {
-                Toast.makeText(QuizActivity.this, "你真棒！", Toast.LENGTH_LONG).show();
+            public void onClick(View v) {
+                ShowToast(R.string.correct_toast);
             }
         });
 
+        mFalseBtn.setOnClickListener(new View.OnClickListener() {
 
-        mFalseBtn = (Button) findViewById(R.id.falseBtn);
+            @Override
+            public void onClick(View v) {
+                ShowToast(R.string.incorrect_toast);
 
+            }
+        });
 
+    }
 
+    private void ShowToast(int resId) {
+        Toast.makeText(QuizActivity.this, resId, Toast.LENGTH_LONG).show();
     }
 }
